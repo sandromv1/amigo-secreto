6b25eb61cf3123d8f0ad1fed4e72c16db2832a06
sandromv1 <sandroprogra@gmail.com>
2025-02-03 15:43:33 -0500

    Actualiza app.js
    Se crea la funcion para agregar amigos. Esta esto vinculado al boton añadir

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigosIncluidos = document.getElementById('amigo').value;
+    
+    if (amigosIncluidos == "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        // Se incluye al amigo en la lista
+        amigos.push(amigosIncluidos);
+        limpiarCaja();
+    }
+
+    console.log(amigos); // Ahora imprime el array actualizado
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
